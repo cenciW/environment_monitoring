93a6e10906ca698574d2740436cdad57b751c525
Henrique Souza <henriquesouzaoficial004@gmail.com>
2024-08-01 21:48:08 -0300

    all implemented !

--- monitoramento_ambiente/monitoramento_ambiente.ino
+++ monitoramento_ambiente/monitoramento_ambiente.ino
@@ -4,131 +4,228 @@
 #include <Wire.h>
 
 // Constants
-#define DHTPIN 2       // Pin where the DHT22 is connected
-#define HL69PIN A0     // Pin where the soil moisture sensor is connected
-#define DHTTYPE DHT22  // DHT 22 (AM2302)
+#define DHTPIN 5      // Pin where the DHT22 is connected
+#define DHTTYPE DHT22 // DHT 22 (AM2302)
+#define BTN_ESQ 17
+#define BTN_DIR 16
+#define BTN_OK 4
+#define LDRPIN 36
+#define LEDPIN 2
 
 // Initialize DHT sensor
 DHT dht(DHTPIN, DHTTYPE);
 
 // Initialize LCD
-LiquidCrystal_I2C lcd(0x27, 16, 2);  // Set the LCD address to 0x27 for a 16 chars and 2 line display, please check your LCD
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Set the LCD address to 0x27 for a 16 chars and 2 line display, please check your LCD
 
 // Variables
-float h;  // Stores humidity value
-float t;  // Stores temperature value
+float h; // Stores humidity value
+float t; // Stores temperature value
 // int soil_moisture;  // Stores soil moisture value
-uint32_t timer = 0;  // Timer for readings
+uint32_t timer = 0; // Timer for readings
 
-// Calibration values for the soil moisture sensor
-// int soilMoistureMin = 1005;  // Replace with your dry soil value -> next 1023
-// int soilMoistureMax = 238;   // Replace with your wet soil value -> next 0
-
-// Linearization coefficients
-const float slope = -0.1303;
-const float intercept = 130.7865;
+float setPointLuminosity = 0;
+float setPointTemp = 22;
 
 // States
-enum State {
-  MONITOR_TEMP_UMID,
-  SHOW_TEMP,
-  // MONITOR_SOIL,
-  // UPDATE_SOIL
+enum State
+{
+  STAND_BY,
+  MONITORAR_AMBIENTE,
+  SAIR,
+  ESCOLHE_TEMP,
+  DEFINE_TEMP
 };
 
 State currentState = MONITOR_TEMP_UMID;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   Serial.println("Temperature, Humidity Sensor Test");
 
-  pinMode(HL69PIN, INPUT);
+  pinMode(BTN_ESQ, INPUT_PULLUP);
+  pinMode(BTN_DIR, INPUT_PULLUP);
+  pinMode(BTN_OK, INPUT_PULLUP);
+  pinMode(LDRPIN, INPUT_PULLUP);
+  pinMode(LEDPIN, OUTPUT);
 
   dht.begin();
-  lcd.init();       // Initialize the LCD
-  lcd.backlight();  // Turn on the backlight
-
-  // // Calibration instructions
-  // Serial.println("Calibration Instructions:");
-  // Serial.println("1. Place the sensor in dry soil and note the raw value.");
-  // Serial.println("2. Place the sensor in wet soil and note the raw value.");
-  // Serial.println("Update the soilMoistureMin and soilMoistureMax constants in the code.");
+  lcd.init();      // Initialize the LCD
+  lcd.backlight(); // Turn on the backlight
 }
 
-void loop() {
-  switch (currentState) {
-    case MONITOR_TEMP_UMID:
-      monitorTemperature();
-      break;
-    case SHOW_TEMP:
-      showTemperature();
-      break;
-      // case MONITOR_SOIL:
-      //   monitorSoilMoisture();
-      //   break;
-      // case UPDATE_SOIL:
-      //   updateSoilMoisture();
-      //   break;
+void stand_by()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Monitoramento");
+  lcd.setCursor(0, 1);
+  lcd.print("Ambiente");
+  if (BTN_OK == HIGH)
+  {
+    currentState = MONITORAR_AMBIENTE;
   }
 }
 
-void monitorTemperature() {
-  if (millis() - timer >= 2000) {
-    h = dht.readHumidity();
-    t = dht.readTemperature();
-    currentState = SHOW_TEMP;
+void monitorar_ambiente()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Para setar temp");
+  lcd.setCursor(0, 1);
+  lcd.print("Press > e OK");
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Para sair");
+  lcd.setCursor(0, 1);
+  lcd.print("Press < e OK");
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Temp Atual:");
+  lcd.setCursor(12, 0);
+  lcd.print(monitorTemperature());
+  lcd.setCursor(0, 0);
+  lcd.print("Humid Atual:");
+  lcd.setCursor(13, 0);
+  lcd.print(monitorHumidity());
+
+  if (monitorLuminosity() <= setPointLuminosity)
+  {
+    digitalWrite(LEDPIN, HIGH);
+  }
+  else
+  {
+    digitalWrite(LEDPIN, LOW);
+  }
+
+  if (BTN_ESQ == HIGH)
+  {
+    currentState = SAIR;
+  }
+  if (BTN_DIR == HIGH)
+  {
+    currentState = ESCOLHE_TEMP;
+  }
+}
+
+void sair()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Press OK ");
+  lcd.setCursor(0, 1);
+  lcd.print("Para sair!");
+  if (BTN_OK == HIGH)
+  {
+    currentState = STAND_BY;
+  }
+  if (BTN_DIR == HIGH)
+  {
+    currentState = MONITORAR_AMBIENTE;
+  }
+  if (BTN_ESQ == HIGH)
+  {
+    currentState = ESCOLHE_TEMP;
+  }
+}
+
+void escolhe_temp()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Setar Temp:");
+  lcd.setCursor(0, 1);
+  lcd.print("Press OK");
+  if (BTN_OK == HIGH)
+  {
+    currentState = DEFINE_TEMP;
+  }
+  if (BTN_DIR == HIGH)
+  {
+    currentState = SAIR;
+  }
+  if (BTN_ESQ == HIGH)
+  {
+    currentState = MONITORAR_AMBIENTE;
+  }
+}
+
+void define_temp()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("< - | ");
+  lcd.setCursor(6, 0);
+  lcd.print(setPointTemp);
+  lcd.setCursor(8, 0);
+  lcd.print(" | + >");
+  lcd.setCursor(0, 1);
+  lcd.print("Press OK");
+
+  if (BTN_DIR == HIGH)
+  {
+    setPointTemp++;
+    if (setPointTemp >= 26) {
+      setPointTemp = 26;
+    }
+  }
+  if (BTN_ESQ == HIGH)
+  {
+    setPointTemp--;
+    if (setPointTemp <= 18) {
+      setPointTemp = 18;
+    }
+  }
+  if (BTN_OK == HIGH)
+  {
+    currentState = MONITORAR_AMBIENTE;
+  }
+}
+
+void loop()
+{
+  switch (currentState)
+  {
+  case STAND_BY:
+    stand_by();
+    break;
+  case MONITORAR_AMBIENTE:
+    monitorar_ambiente();
+    break;
+  case SAIR:
+    sair();
+    break;
+  case ESCOLHE_TEMP:
+    escolhe_temp();
+    break;
+  case DEFINE_TEMP:
+    define_temp();
+    break;
+  }
+}
+
+float monitorHumidity()
+{
+  if (millis() - timer >= 2000)
+  {
     timer = millis();
+    return h = dht.readHumidity();
   }
 }
 
-void showTemperature() {
-  Serial.print("Humidity:");
-  Serial.print(h);
-  Serial.print(" %, Temp:");
-  Serial.print(t);
-  Serial.println(" °C");
-
-  currentState = MONITOR_TEMP_UMID;
+float monitorTemperature()
+{
+  if (millis() - timer >= 2000)
+  {
+    timer = millis();
+    return t = dht.readTemperature();
+  }
 }
 
-// void monitorSoilMoisture() {
-//   if (millis() - timer >= 2000) {
-//     soil_moisture = analogRead(HL69PIN);
-//     currentState = UPDATE_SOIL;
-//     timer = millis();
-//   }
-// }
-
-// void updateSoilMoisture() {
-
-//   // int soilMoisturePercent = round(slope * soil_moisture + intercept);
-//   // soilMoisturePercent = constrain(soilMoisturePercent, 0, 100);
-//   // float soil_moisture_percent_invert = (1.00 - soil_moisture/1023)*100.00;
-
-//   // Serial.print("Soil Moisture: ");
-//   //Serial.print(soil_moisture);
-//   // Serial.print(soil_moisture);
-//   // Serial.print(soilMoisturePercent);
-//   // Serial.print(soil_moisture_percent_invert);
-//   // Serial.println("%)");
-
-//   lcd.clear();
-//   lcd.setCursor(0, 0);
-//   lcd.print("T:");
-//   lcd.print(t, 1); // One decimal place
-//   lcd.print("C");
-
-//   lcd.setCursor(8, 0);
-//   lcd.print(" H:");
-//   lcd.print(h, 1); // One decimal place
-//   lcd.print("%");
-
-//   // lcd.setCursor(0, 1);
-//   // lcd.print("Solo: ");
-//   // lcd.print(soil_moisture_percent_invert);
-//   // lcd.print("% (");
-//   // lcd.print(soil_moisture);
-//   // lcd.print(")");
-
-//   currentState = MONITOR_TEMP_UMID;
-// }
+float monitorLuminosity()
+{
+  return analogRead(LDRPIN);
+}
